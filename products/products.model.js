ccb52a394c9c9855980af4294c58be1390035a21
ChristopherL <anbchris@gmail.com>
2025-07-13 22:56:16 -0400

    implementing mutations in graphql

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
         id: 'redshoe',
         description: 'Red Shoe',
         price: 42.12,
+        reviews: [],
     },
     {
         id: 'bluejean',
         description: 'Blue Jeans',
         price: 55.55,
+        reviews: [],
     }
 ];
 
@@ -15,6 +17,49 @@ function getAllProducts() {
     return products;
 }
 
+function getProductsByPrice(min, max) {
+    return products.filter(product => {
+        return product.price >= min && product.price <= max;
+    });
+}
+
+function getProductByID(id) {
+    return products.find(product => {
+        return product.id === id;
+    });
+}
+
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: [],
+    };
+
+    products.push(newProduct);
+    return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+    const matchedProduct = products.find(product => product.id === id);
+
+    if (matchedProduct) {
+        const newReview = {
+            id, 
+            rating, 
+            comment,
+        }
+    
+        matchedProduct.reviews.push(newReview);
+        return newReview;
+    }
+}
+
 module.exports = {
     getAllProducts,
+    getProductsByPrice,
+    getProductByID,
+    addNewProduct,
+    addNewProductReview,
 }
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const path = require('path');
 const express = require('express');
-const { createYoga } = require('graphql-yoga');
+const { ApolloServer } = require('@apollo/server');
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const typesArray = loadFilesSync( './**/*', { extensions: ['graphql'] });
 const resolversArray = loadFilesSync('./**/*', { extensions: ['resolvers.js'] });
 
-const schema = makeExecutableSchema({
-    typeDefs: typesArray,
-    resolvers: resolversArray,
-});
+async function startApolloServer() {
+    const app = express();
 
-const app = express();
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray,
+    });
 
-app.use('/graphql', createYoga({
-    schema,
-    graphiql:  true,
-}));
+    const server = new ApolloServer({})
+}
+
+app.use('/graphql', () => {});
 
 app.listen(3000, () => {
     console.log(`listening on port 3000`);
